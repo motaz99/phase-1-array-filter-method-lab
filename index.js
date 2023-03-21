2689e55839b6acca3b32faeaaf6a1cf2bc05e3f8
motaz <motaz.dn.ali@gmail.com>
2023-03-21 17:13:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+const findMatching = (drivers, str) =>
+  drivers.filter((driver) => driver.toUpperCase() === str.toUpperCase());
+
+const fuzzyMatch = (drivers, str) =>
+  drivers.filter((driver) => driver.slice(0, str.length) === str);
+
+const matchName = (drivers, str) =>
+  drivers.filter((driver) => driver.name === str);
